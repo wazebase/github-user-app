b59549ef21c228907ea8950c059130415e7931f8
wazebase <nikita.menkov@gmail.com>
2021-06-25 20:37:11 +0200

    delete token

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const API_URL = 'https://api.github.com/users';
-export const TOKEN = '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';
 
 export class Token {
     private token:string
